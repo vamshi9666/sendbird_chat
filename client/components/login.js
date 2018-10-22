6318b8785f1c70d0468218cb5eca8ba0b1a8746b
vamshi9666 <vamshi9666@riseup.net>
2018-10-22 19:42:22 +0530

    btn click enabled to send message

--- client/components/login.js
+++ client/components/login.js
@@ -1,88 +1,84 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import propTypes from 'prop-types';
-import { connect } from 'react-redux'
-import { sbLogin } from '../actions/login';
-import {
-    FormControl,
-    FormGroup,
-    ControlLabel,
-    Button
-} from 'react-bootstrap';
+import {connect} from 'react-redux';
+import {sbLogin} from '../actions/login';
+import {FormControl, FormGroup, ControlLabel, Button} from 'react-bootstrap';
 
 const formGroup = {
-    'textAlign': 'center',
-    'marginLeft': '20%',
-    'marginRight': '20%',
-    'display': 'flex',
-    'flexDirection': 'column'
-}
+  textAlign: 'center',
+  marginLeft: '20%',
+  marginRight: '20%',
+  display: 'flex',
+  flexDirection: 'column',
+};
 
-const btnCus ={
-    'width': '100px',
-}
+const btnCus = {
+  width: '100px',
+};
 
 class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userId: '',
-            nickname: ''
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this)
-    }
-    onSubmit(e){
-        e.preventDefault();
-        this.props.sbLogin(this.state)
-        this.context.router.push('/home')
-    }
-        
-    
-    handleChange(e) {
-        this.setState({ [e.target.name]: e.target.value });
-    }
-    render() {
-        return (
-            <div style={formGroup}>
-                <form onSubmit={this.onSubmit}>
-                    <FormGroup>
-                        <ControlLabel>User Name</ControlLabel>
-                        <FormControl
-                            bsSize="sm"
-                            name="userId"
-                            type="text"
-                            value={this.state.username}
-                            placeholder="Enter text"
-                            onChange={this.handleChange} />
-                        <FormControl.Feedback />
-                    </FormGroup>
-                    <FormGroup>
-                        <ControlLabel>Nick Name</ControlLabel>
-                        <FormControl
-                            name="nickName"
-                            type="text"
-                            value={this.state.nickName}
-                            placeholder="Enter text"
-                            onChange={this.handleChange} />
-                    </FormGroup>
-                    <Button type="submit" style={btnCus} >
-                        Sign in
-                    </Button>
-                </form>
-            </div>
-        )
-    }
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: '',
+      nickname: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    this.props.sbLogin(this.state);
+    this.context.router.push('/home');
+  }
 
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value});
+  }
+  render() {
+    return (
+      <div style={formGroup}>
+        <form onSubmit={this.onSubmit}>
+          <FormGroup>
+            <ControlLabel>User Name</ControlLabel>
+            <FormControl
+              bsSize="sm"
+              name="userId"
+              type="text"
+              value={this.state.username}
+              placeholder="Enter text"
+              onChange={this.handleChange}
+            />
+            <FormControl.Feedback />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Nick Name</ControlLabel>
+            <FormControl
+              name="nickName"
+              type="text"
+              value={this.state.nickName}
+              placeholder="Enter text"
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <Button type="submit" style={btnCus}>
+            Sign in
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
 
 Login.contextTypes = {
-    router: propTypes.object.isRequired
-}
+  router: propTypes.object.isRequired,
+};
 
 Login.propTypes = {
-    sbLogin : propTypes.func.isRequired
-}
-const mapStateToProps = ( ) => {
-
-}
-export default connect(null, { sbLogin }) (Login);
+  sbLogin: propTypes.func.isRequired,
+};
+const mapStateToProps = () => {};
+export default connect(
+  null,
+  {sbLogin},
+)(Login);
--- client/components/messageform.js
+++ client/components/messageform.js
@@ -1,58 +1,66 @@
-import { connect } from 'react-redux';
-import React, { Component } from 'react';
-import propTypes  from 'prop-types';
-import { sendMessage } from '../actions/message';
-import { 
-    Button
-} from 'react-bootstrap'
+import {connect} from 'react-redux';
+import React, {Component} from 'react';
+import propTypes from 'prop-types';
+import {sendMessage} from '../actions/message';
+import {Button} from 'react-bootstrap';
 const inputStyle = {
-    'width':'75vh',
-    'marginRight': '20px'
-
-}
+  width: '75vh',
+  marginRight: '20px',
+};
 
 class MessageForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: null,
-            message_input: ''
-        };
-        this.sendMessage = this.sendMessage.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      message_input: '',
+    };
+    this.sendMessage = this.sendMessage.bind(this);
+  }
 
-    sendMessage(e){
-        e.preventDefault();
-        this.props.sendMessage(this.state.message_input);
-        this.setState({
-            message_input: ''
-        })
-
-    }
-    handleChange(e){
-        e.preventDefault();
-        this.setState({
-            message_input:e.target.value
-        })
-    }
-    render() {
-        return (
-            <div className="message-form" >
-                <form onSubmit={this.sendMessage} >
-                    <input  style={inputStyle} type="text" value={this.state.message_input} onChange={this.handleChange.bind(this)} name="name" />
-                </form>
-                <Button className="btn btn-success" type="submit"> </Button>
-            </div>
-        )
-    }
+  sendMessage(e) {
+    e.preventDefault();
+    this.props.sendMessage(this.state.message_input);
+    this.setState({
+      message_input: '',
+    });
+  }
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({
+      message_input: e.target.value,
+    });
+  }
+  render() {
+    return (
+      <div className="message-form">
+        <form onSubmit={this.sendMessage}>
+          <input
+            style={inputStyle}
+            type="text"
+            value={this.state.message_input}
+            onChange={this.handleChange.bind(this)}
+            name="name"
+          />
+        </form>
+        <Button
+          className="btn btn-success"
+          onClick={this.sendMessage}
+          type="submit">
+          {' '}
+        </Button>
+      </div>
+    );
+  }
 }
-const mapStateToProps = (state) => {
-    return {
-        
-    }
-}
+const mapStateToProps = state => {
+  return {};
+};
 
 MessageForm.propTypes = {
-    sendMessage : propTypes.func.isRequired
-}
-export default  connect(null, { sendMessage }) (MessageForm);
+  sendMessage: propTypes.func.isRequired,
+};
+export default connect(
+  null,
+  {sendMessage},
+)(MessageForm);
